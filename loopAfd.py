3abbfe63d63a107efb70d868240583beadcb1383
Ruan <pm.ruan@gmail.com>
2015-10-14 16:57:27 -0600

    Algoritmo

--- loopAfd.py
+++ loopAfd.py
@@ -1,52 +1,46 @@
+#!/usr/bin/env python
 sigma = [0,1] #lista de simbolos do AFD
 estados = [1,2,3,4,5,6]
-programa =[[1,0,6],
-           [1,1,2],
-           [2,1,5],
-           [2,0,3],
+programa =[[1,0,2], #[Estado atual, simbolo, estado alcançado]
+           [1,1,3],
+           [2,1,4],
+           [2,0,2],
            [3,1,4],
-           [3,0,4],
-           [4,1,3],
-           [4,0,3],
-           [5,0,3],
-           [5,1,3],
-           [6,0,5],
-           [6,1,3]]
+           [3,0,1],
+           [4,1,5],
+           [4,0,1],
+           [5,0,0],
+           [5,1,6],
+           [6,0,0],
+           [6,1,0]]
 Ei = 1 #estado inicial
-Ef = [5] #Lista de Estados finais do automato
+Ef = [6] #Lista de Estados finais do automato
 lEa = []  #lista de estados alcançados a partir de Ei
 count = [0,0,0,0,0,0] #lista que conta o numero de vezes que o estado é alcancado
 lEa.append(Ei)
 
 def main():
-    loop = False #identifica a existencia de loop no automato
-    lAs = [] #lista de estados alcançaveis a partir do estado atual
-    while(len(lEa)!=0):
-        print(lEa)
-        inc = False
-        Ea = lEa.pop() #estado atual
-        lAs.append(Ea)
-        for i in sigma:
+    lAs = [] #lista de estados alcançaveis a partir do estado atual(Ea)
+    while(len(lEa)!=0): #verifica se a lista de estados alcançados esta vazia
+        #print(lEa)
+        Ea = lEa.pop() #coloca o ultimo elemento da lista na variavel de estado atual(Ea)
+        for i in sigma: #verifica os estados alcançaveis a partir do estado atual com cada simbolo do alfabeto
             Esa = transicao(Ea,sigma[i]) #estado alcancado a partir de Ea
-            if(Esa != 0): 
-                if(Esa not in lAs):
-                    count[Esa -1] = count[Esa -1] + 1
-                    lEa.append(Esa)
-                    lAs.append(Esa)
-                    inc = True
-                if(count[Esa -1]>1 and inc == True):
-                    if(count[Esa-1]>2): 
-                        while(Esa in lEa):lEa.remove(Esa)
-                    loop = True 
-            if(Esa in Ef and loop):
+            if(Esa != 0):
+                if(Esa not in lAs): #se o estado ainda não foi alcançado partindo de Ea
+                    count[Esa -1] = count[Esa -1] + 1 #numero de vezes em que um estado foi alcançado
+                    lEa.append(Esa) #adiciona Esa a lista de estados alcançados a partir de Ei
+                    lAs.append(Esa) #adiciona Esa a lista de estados alcançados a partir de Ea
+                if(count[Esa-1]>2): #se o estado atual estiver em loop
+                    while(Esa in lEa):lEa.remove(Esa) #remove todos os Esa que entram em loop
+            if(Esa in Ef and count[Ea-1]>1): #se o estado Alcançado por Ea for Estado final e se o estado atual possuir loop
                 print("Automato possui loop")
                 return 1
-        lAs = []    
+        lAs = [] #zera a lista de Estados alcançados a partir de Ea para iniciar a computação
     print("Automato não possui loop")
 
 
-def transicao(Eatual,sig):
-    transicoes = [] #possiveis transicoes
+def transicao(Eatual,sig): #retorna o estado que é alcançado a partir do Eatual com o simbolo sig
     for i in programa:
         if(i[0] == Eatual and i[1] == sig):
             return i[2]
